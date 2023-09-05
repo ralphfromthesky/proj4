9f6053dc92ed7a322b1b7023e64673648cde9dc0
ybkja26 <ybkja26@gmail.com>
2023-09-05 17:39:21 +0800

    update

--- main.js
+++ main.js
@@ -8,9 +8,21 @@ const slideDown = () => {
 arrow.addEventListener('click', slideDown);
 
 const arrow3 = document.getElementById('arrow3');
-const sidebars  = document.getElementById('sidebars')
+const X = document.getElementById('X')
+const sidebars = document.getElementById('sidebars')
 
 const slideToRight = () => {
     sidebars.classList.toggle('slide-to-right');
+    if (arrow3.style.display === 'none') {
+        arrow3.style.display = 'block';
+        X.style.display = 'none';
+    } else {
+        X.style.display = 'block';
+        arrow3.style.display = 'none';
+    }
 }
+
+
+
 arrow3.addEventListener('click', slideToRight)
+X.addEventListener('click', slideToRight)
